e5c619ff8a5d3311e6d495263670e5b39146ce3a
sangeeth301 <144532263+sangeeth301@users.noreply.github.com>
2025-02-24 17:47:28 +0530

    Update createInspectionSheet.js

--- createInspectionSheet.js
+++ createInspectionSheet.js
@@ -1,6 +1,5 @@
 import { LightningElement, track } from 'lwc';
 import saveInspectionSheet from '@salesforce/apex/CreateInspectionSheet.saveInspectionSheetRecord';
-import NavigateInspectionSheet from '@salesforce/apex/CreateInspectionSheet.navigateToRecordPage';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import modal from '@salesforce/resourceUrl/PPM_Size';
 import myImage from '@salesforce/resourceUrl/Haulbrooke';
@@ -10,6 +9,11 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class CreateInspectionSheet extends NavigationMixin(LightningElement) {
     myImageUrl = myImage;
      @track isModalOpen = true;
+     @track isSignatureAdded = false;
+    @track showSignaturePad = false;
+
+    signaturePad;
+    drawing = false;
     @track inspectionSheetRecord = {
         Shop_Name__c: '',
         Name_Of_Motor_Carrier__c: '',
@@ -24,6 +28,7 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
         All_Devices__c: '', Conspicuity_Tape__c: '', Protection_Against_Shifting_Cargo__c: '', Condition_Of_Loading__c: '', Steering_Wheel_Free_Play__c: '', Steering_Column__c: '', 
         Front_Axie_Beam_Components__c: '', Steering_Gear_Box__c: '', Pitman_Arm__c: '', Power_Steering__c: '', Ball_and_Socket_Joints__c: '', Tie_Rods_and_Drag_Links__c: '', Steering_System__c: '', Nuts__c: '', Frame_Members__c: '', Tire_and_Wheel_Clearance__c: '', Adjustable_Axle_Assemblies__c: '', Damage__c: '', Electrical__c: '', Box_Skin__c: '', U_Bolts_Torque_50_llb_ft__c: '', Inspect_Welds_For_Cracking__c: '', Door_Roller_Inspect_and_Lube__c: '', Hinge_Inspect_and_Lube__c: '', Door_Operation_Seals__c: '', Springs_Air_Bags_Height__c: '', Spring_Hangers__c: '', Front_U_Bolt_Torque_270_360_lb_ft__c: '', Rear_U_Bolt_Torque_420_500_lb_ft__c: '', Torque_Radius_or_Tracking_Components__c: '', Inspect_Replace_Cabin_Air_Filter__c: '', Lock_or_Side_Ring__c: '', Welds__c: '', Wheels_and_Rims__c: '', Studs_and_Lug_Nuts__c: '', Wheel_Bearings__c: '', Axle_Seals__c: '', Mudflaps__c: '', Triangles__c: '', Extinguisher__c: '', Placards_Holders__c: '', Horn__c: '', Steering_Axle__c: '', All_Other_Tires__c: '', Completed__c: '', Does_the_DOT_Inspection_Decal_Expire__c: '', Completed_DOT_Inspection__c: '', DOT_Inspection_Expiration_Date__c: '', Does_the_State_Inspection_Decal_Expire__c: '', Completed_State_Inspection__c: '', State_Inspection_Sticker_Expiration_Date__c: '', Comments__c: '', Digital_Signature__c: '', Printed_Name_of_Inspector__c: '', Signature_of_Inspector__c: '', LF_Tires__c: '', LF_Brakes__c: '', LFO_Tires__c: '', LFO_Brakes__c: '', LFI_Tires__c: '', LFI_Brakes__c: '', LRO_Tires__c: '', LRO_Brakes__c: '', LRI_Tires__c: '', LRI_Brakes__c: '', RF_Tires__c: '', RF_Brakes__c: '', RFO_Tires__c: '', RFO_Brakes__c: '', RFI_Tiers__c: '', RFI_Breaks__c: '', RRO_Tires__c: '', RRO_Breaks__c: '', RRI_Tires__c: '', RRI_Brakes__c: ''
     };
+
     handleInputChange(event) {
         const fieldName = event.target.dataset.field; 
         const isCheckbox = event.target.type === 'checkbox'; 
@@ -59,6 +64,147 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
     }
 }
 
+    initSignatureCanvas() {
+        console.log('OUTPUT : init signature canvas method called');
+        var canvas, ctx, flag = false,
+            prevX = 0,
+            currX = 0,
+            prevY = 0,
+            currY = 0,
+            dot_flag = false;
+
+        var x = "#0000FF",
+            y = 2,
+            w, h;
+        canvas = this.template.querySelector('canvas');
+        var ratio = Math.max(window.devicePixelRatio || 1, 1);
+        w = canvas.width * ratio;
+        h = canvas.height * ratio;
+        ctx = canvas.getContext("2d");
+
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+        // Set up touch events for mobile, etc
+        canvas.addEventListener("touchstart", function (e) {
+            var touch = e.touches[0];
+            console.log('touch start:=' + touch);
+            var mouseEvent = new MouseEvent("mousedown", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            canvas.dispatchEvent(mouseEvent);
+            e.preventDefault();
+        }, false);
+        canvas.addEventListener("touchend", function (e) {
+            var mouseEvent = new MouseEvent("mouseup", {});
+            canvas.dispatchEvent(mouseEvent);
+        }, false);
+        canvas.addEventListener("touchmove", function (e) {
+            var touch = e.touches[0];
+            var mouseEvent = new MouseEvent("mousemove", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            canvas.dispatchEvent(mouseEvent);
+            e.preventDefault();
+
+        }, false);
+
+        // Get the position of a touch relative to the canvas
+        function getTouchPos(canvasDom, touchEvent) {
+            var rect = canvasDom.getBoundingClientRect();
+            return {
+                x: touchEvent.touches[0].clientX - rect.left,
+                y: touchEvent.touches[0].clientY - rect.top
+            };
+        }
+
+        function findxy(res, e) {
+            const rect = canvas.getBoundingClientRect();
+            if (res == 'down') {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - rect.left;
+                currY = e.clientY - rect.top;
+
+                flag = true;
+                dot_flag = true;
+                if (dot_flag) {
+                    ctx.beginPath();
+                    ctx.fillStyle = x;
+                    ctx.fillRect(currX, currY, 2, 2);
+                    ctx.closePath();
+                    dot_flag = false;
+                }
+            }
+            if (res == 'up' || res == "out") {
+                flag = false;
+            }
+            if (res == 'move') {
+                if (flag) {
+                    prevX = currX;
+                    prevY = currY;
+                    currX = e.clientX - rect.left;
+                    currY = e.clientY - rect.top;
+                    draw(canvas, ctx);
+                }
+            }
+        }
+        function draw() {
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(currX, currY);
+            ctx.strokeStyle = x;
+            ctx.lineWidth = y;
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+    }
+
+    // Toggle Signature Pad
+    toggleSignaturePad(event) {
+        this.isSignatureAdded = event.target.checked;
+        this.showSignaturePad = event.target.checked;
+        if (this.showSignaturePad) {
+            setTimeout(() => {
+                this.initSignatureCanvas();
+            }, 0);
+        }
+    }
+
+    // Close Signature Modal
+    closeSignaturePad() {
+        this.showSignaturePad = false;
+        this.isSignatureAdded = false;
+    }
+
+    // Clear Signature
+    clearSignature() {
+        const canvas = this.template.querySelector('canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // Save Signature
+    saveSignature() {
+        const canvas = this.template.querySelector('canvas');
+        const signatureData = canvas.toDataURL('image/png');
+        this.inspectionSheetRecord.Digital_Signature__c = `<img src="${signatureData}" />`;
+        this.showSignaturePad = false;
+    }
+
+
     validateFields() {
     const validations = [
         {
@@ -70,7 +216,14 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
             field: 'Unit__c',
             condition: (value) => !value || /^[0-9]+$/.test(value),
             errorMessage: 'Unit must contain only numeric values or be left empty.'
-        }
+        }/*,
+       {
+            field: 'Digital_Signature__c',
+            condition: (value) => value ,
+            errorMessage: 'Mechanic signature is required.',
+            required: true
+        }*/
+
     ];
 
     for (const { field, condition, errorMessage } of validations) {
@@ -110,6 +263,7 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
                         variant: 'success',
                     })
                 );
+                 this.clearFormFields();
                 this[NavigationMixin.Navigate]({
                     type: 'standard__recordPage',
                     attributes: {
@@ -118,6 +272,7 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
                         actionName: 'view',
                     },
                 });
+              
             })
             
             .catch((error) => {
@@ -131,10 +286,23 @@ export default class CreateInspectionSheet extends NavigationMixin(LightningElem
                 );
             });
     }
+
+   clearFormFields() {
+    const inputFields = this.template.querySelectorAll('lightning-input');
+    inputFields.forEach(field => {
+        if (field.type === 'checkbox') {
+            field.checked = false; // Reset checkbox
+        } else {
+            field.value = ''; // Reset text inputs
+        }
+    });
+    }
+
+
   handleNavigateBack(event) {
     console.log("Dismissing modal", event.detail);
-     window.location.href = '/lightning/o/Inspection_Sheet__c/list?filterName=__Recent'; 
-    //history.back();  //location.reload();
+    this.clearFormFields();
+    window.history.back();
     }
 
 
